b54d8ee586f6cb1e8285de3d1f4c863bc96858c1
Caleb Crane <github@simulacre.org>
2011-12-28 13:14:15 +0900

    don't let the Haml::Engine filename be turned into symbol

--- lib/awestruct/hamlable.rb
+++ lib/awestruct/hamlable.rb
@@ -4,12 +4,11 @@ module Awestruct
     def render(context)
       rendered = ''
       begin
-        options = (site.haml || {:filename => ".#{relative_source_path}"}).inject({}){|h,(k,v)| h[k.to_sym] = v.to_sym; h }
+        options = (site.haml || {}).inject({}){|h,(k,v)| h[k.to_sym] = v.to_sym; h }
+        options[:filename] = ".#{relative_source_path}" unless options[:filename]
         haml_engine = Haml::Engine.new( raw_page_content, options )
         rendered = haml_engine.render( context )
       rescue => e
-        #lineo = e.backtrace[0].split(":")[1]
-        #e.set_backtrace(e.backtrace[1..-1].unshift("#{self.source_path}:#{lineo}"))
         puts e
         puts e.backtrace
       end
