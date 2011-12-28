4159e3f10b53edc09c6f0ab48aca75dd466fb204
Caleb Crane <github@simulacre.org>
2011-12-28 13:01:27 +0900

    by default give Haml::Engine the filename of the template being used

--- lib/awestruct/hamlable.rb
+++ lib/awestruct/hamlable.rb
@@ -4,12 +4,12 @@ module Awestruct
     def render(context)
       rendered = ''
       begin
-        options = (site.haml || {}).inject({}){|h,(k,v)| h[k.to_sym] = v.to_sym; h }
+        options = (site.haml || {:filename => ".#{relative_source_path}"}).inject({}){|h,(k,v)| h[k.to_sym] = v.to_sym; h }
         haml_engine = Haml::Engine.new( raw_page_content, options )
         rendered = haml_engine.render( context )
       rescue => e
-        lineo = e.backtrace[0].split(":")[1]
-        e.set_backtrace(e.backtrace[1..-1].unshift("#{self.source_path}:#{lineo}"))
+        #lineo = e.backtrace[0].split(":")[1]
+        #e.set_backtrace(e.backtrace[1..-1].unshift("#{self.source_path}:#{lineo}"))
         puts e
         puts e.backtrace
       end
