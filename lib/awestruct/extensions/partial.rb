3c3571f8478fa2eae80fc43af46d002e3e48cb3a
Caleb Crane <github@simulacre.org>
2011-12-31 09:40:13 +0900

    partial helper can take locals

--- lib/awestruct/extensions/partial.rb
+++ lib/awestruct/extensions/partial.rb
@@ -3,8 +3,9 @@ module Awestruct
   module Extensions
     module Partial
 
-      def partial(path)
+      def partial(path, locals = {})
         page = site.engine.load_site_page( File.join( '_partials', path ) )
+        locals.each { |name, value| page.send("#{name}=", value) }
         page.content if ( page )
       end
 
